4a9ff15ba866fef1700e7bd28f02f6a35515682d
Rishabh <rishabhptl2003@gmail.com>
2024-03-16 15:45:14 +0530

    some changes

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -18,7 +18,9 @@ function MapComponent() {
     return () => map.remove();
   }, []);
 
-  return <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />;
+  return ( <>
+   <div ref={mapContainerRef} style={{ width: '100vh', height: '100vh' }} />
+  </> );
 }
 
 export default MapComponent;

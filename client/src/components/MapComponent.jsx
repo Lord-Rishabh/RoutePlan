ded133349e6594ac977246885c794bd71a99f762
Rishabh <rishabhptl2003@gmail.com>
2024-03-16 14:08:52 +0530

    map component added

---
+++ client/src/components/MapComponent.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXRoYXJ2am9zaGkiLCJhIjoiY2x0dHQ3M2lxMTBobjJqcDd5cHRkNjZsZyJ9.o3cmAVRVx5VRlkFbmWb3zQ';
+
+function MapComponent() {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [0, 0],
+      zoom: 5,
+    });
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []);
+
+  return <div ref={mapContainerRef} style={{ width: '100%', height: '100vh' }} />;
+}
+
+export default MapComponent;
